f945321b07e22bd85c01183bc9fe59e96f71c5e3
olawale-o <omoogunolawale@gmail.com>
2023-01-02 06:32:17 +0100

    Set up convict

---
+++ config.js
@@ -0,0 +1,36 @@
+const convict = require('convict');
+
+const config = convict({
+  env: {
+    doc: 'The application environment.',
+    format: ['production', 'development', 'test'],
+    default: 'development',
+    env: 'NODE_ENV',
+  },
+  port: {
+    doc: 'The port to bind.',
+    format: 'port',
+    default: 5000,
+    env: 'PORT',
+    arg: 'port',
+  },
+  db: {
+    host: {
+      doc: 'Database host name',
+      format: '*',
+      default: 'mongodb://localhost:27017/generaldb?retryWrites=true&w=majority',
+    },
+    name: {
+      doc: 'Database name',
+      format: String,
+      default: 'generaldb',
+    },
+  },
+});
+
+const env = config.get('env');
+config.loadFile(`./config/${env}.json`);
+
+config.validate({ allowed: 'strict' });
+
+module.exports = config;
