bda6b375dc25d0157c6a0ebcb974f2609858b004
olawale-o <omoogunolawale@gmail.com>
2023-01-03 04:22:50 +0100

    Listen to socket

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const http = require('http');
+const { Server } = require('socket.io');
+
 const config = require('./config');
 const client = require('./src/database')(config.get('db.host'));
 const dbConnection = require('./src/database/connection');
 const app = require('./src/app');
+const socketConnection = require('./socketConnection');
 
 const server = http.createServer(app);
+
+const IO = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+socketConnection(IO);
 
 dbConnection(client, config.get('db.name'))
   .then((result) => {
