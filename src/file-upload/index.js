307d327dcec7968ea83d1563a42d442e3da88ad4
olawale-o <omoogunolawale@gmail.com>
2023-01-07 17:24:31 +0100

    Add api/v1/fileupload/video-chunk

--- src/file-upload/index.js
+++ src/file-upload/index.js
@@ -5,5 +5,6 @@ const controller = require('./controller');
 const upload = multer({ dest: `${process.cwd()}/storage` });
 
 router.post('/', upload.single('image'), controller.create);
+router.get('/video-chunk', controller.getVideo);
 
 module.exports = router;
