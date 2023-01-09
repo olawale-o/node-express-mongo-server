f5f5cadbba81ed8160e34cc02c9175357764cccb
olawale-o <omoogunolawale@gmail.com>
2023-01-09 16:34:37 +0100

    Implement logic to upload large file with cloudinary

--- src/services/cloudinary-service.js
+++ src/services/cloudinary-service.js
@@ -1,4 +1,5 @@
 const cloudinary = require('cloudinary');
+const streamifier = require('streamifier');
 
 const config = require('../../config');
 
@@ -12,9 +13,41 @@ module.exports = {
   fileUploader: (file, folder = 'chats', resourceType = 'auto') => new Promise((resolve, reject) => {
     cloudinary.v2.uploader.upload(file, { resource_type: resourceType, folder: `${folder}/` })
       .then((result) => resolve(result))
-      .catch((error) => {
-        console.log(error);
+      .catch(() => {
         reject(new Error('Unable to upload file at this time'));
       });
+  }),
+  fileLargeUploader: (file, folder = 'chats', resourceType = 'auto') => new Promise((resolve, reject) => {
+    cloudinary.v2.uploader.upload_large(
+      file,
+      {
+        resource_type: resourceType,
+        chunk_size: 6000000,
+        folder: `${folder}/`,
+      },
+      (err, result) => {
+        if (err) reject(new Error('Unable to upload file at this time'));
+        resolve(result);
+      },
+    );
+  }),
+  uploadFromBuffer: (buffer) => new Promise((resolve, reject) => {
+    const cldUploadStream = cloudinary.v2.uploader.upload_stream(
+      {
+        folder: 'chats',
+        resource_type: 'video',
+        chunk_size: 6000000,
+      },
+      (error, result) => {
+        if (result) {
+          resolve(result);
+        } else {
+          console.log(error);
+          reject(error);
+        }
+      },
+    );
+
+    streamifier.createReadStream(buffer).pipe(cldUploadStream);
   }),
 };
