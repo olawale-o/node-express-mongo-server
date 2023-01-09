f652b5476060a76ef0aef000830c7ab2fb816033
olawale-o <omoogunolawale@gmail.com>
2023-01-09 16:35:17 +0100

    Add endpoint to generate signature for cloudinary

--- src/file-upload/index.js
+++ src/file-upload/index.js
@@ -4,8 +4,11 @@ const controller = require('./controller');
 
 const upload = multer({ dest: `${process.cwd()}/storage` });
 
+const multerStorage = require('../middlewares/multer-storage');
+
 router.post('/', upload.single('image'), controller.create);
 router.get('/video-chunk', controller.getVideo);
-router.post('/:id/videos', controller.newVideoUpload);
+router.post('/:id/videos', multerStorage.single('video'), controller.newVideoUpload);
+router.get('/signature', controller.getSignature);
 
 module.exports = router;
