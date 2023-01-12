d8463ac18e97599f41ea4ef7fa5a4f8bcb130800
olawale-o <omoogunolawale@gmail.com>
2023-01-12 02:15:23 +0100

    Fix invalid credential error and update verifyRefereshToken method in user service

--- src/user/service.js
+++ src/user/service.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const repo = require('./repository');
 const AppError = require('../common/app-error');
+const tokenService = require('../services/token-service');
 
 const verifyUserName = async (crendentials) => repo.findByUsernameOrEmail(crendentials);
 const verifyPassword = async ({ password, passwordEncrypt }) => (
@@ -25,6 +26,9 @@ module.exports = {
   },
   login: async (credentials) => {
     const user = await verifyUserName(credentials);
+    if (!user) {
+      throw new AppError(422, 'Invalid credentials');
+    }
     if (!(await
     verifyPassword({ password: credentials.password, passwordEncrypt: user.password })
     )) {
@@ -32,10 +36,28 @@ module.exports = {
     }
     return user;
   },
+  findUserBy: async (credentials) => repo.findUserBy(credentials),
   getToken: async (credentials) => repo.findById(credentials),
   updateToken: async (credentials, operation) => repo.findByIdAndUpdate(credentials, operation),
   getOtherUsers: async (options) => repo.findAllUsers(options),
   updateProfile: async (credentials, operation, options) => (
     repo.findByIdAndUpdate(credentials, operation, options)
   ),
+  verifyRefereshToken: async (credentials) => {
+    const foundUser = await repo.findUserBy(credentials);
+    if (!foundUser) {
+      return new AppError(401, 'Unauthorized');
+    }
+    const payload = await tokenService.verifyRefreshToken(credentials.refreshToken);
+    if (!payload) {
+      return new AppError(403, 'Forbidden');
+    }
+    // eslint-disable-next-line no-underscore-dangle
+    if (payload.userId !== foundUser._id.toString()) {
+      return new AppError(403, 'Forbidden');
+    }
+    // eslint-disable-next-line no-underscore-dangle
+    const accessToken = await tokenService.signAccessToken({ userId: foundUser._id });
+    return accessToken;
+  },
 };
